1c8405b26cd9cbbe555464f90ecae6d729503b45
hanbing <6736770907>
2019-08-16 15:22:49 +0800

    冒泡和选择排序ok

---
+++ src/main/java/com/yyh/algorithm/sort/Insertion.java
@@ -0,0 +1,21 @@
+package com.yyh.algorithm.sort;
+
+/**
+ * @author pc-d
+ */
+public class Insertion extends Sort {
+    public static void sort(int[] arr) {
+        for (int i = 1; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+
+                }
+
+            }
+
+
+        }
+    }
+}
+
